224478c3b56c09c6c1faafef71646e015e485229
NataliaVozhdaeva <LomzNat@yandex.ru>
2024-05-14 21:29:35 +0400

    feat: add class active for menu items

--- customized/my-js.js
+++ customized/my-js.js
@@ -1,33 +1,18 @@
 const menuItems = document.querySelectorAll('.nav-link');
 
+location.hash = 'home';
 document.querySelector('a[href="#home"]').classList.add('active');
 
 menuItems.forEach((el) => {
   el.addEventListener('click', function appearModal(e) {
     menuItems.forEach((el) => el.classList.remove('active'));
     setTimeout(function () {
-      menuItems.forEach((el) => {
-        if (location.hash === el.href) el.classList.add('active');
-      });
-
-      // menuItems.forEach((el) => el.classList.remove('active'));
-    }, 0);
+      for (let i = 0; i < menuItems.length; i++) {
+        if (location.hash === '#' + el.href.split('#')[1]) {
+          el.classList.add('active');
+          return;
+        }
+      }
+    }, 10);
   });
 });
-
-/* switch (location.hash) {
-        case '#started':
-          e.target.closest('a').classList.add('active');
-          break;
-        case '#develop':
-          e.target.closest('a').classList.add('active');
-          break;
-        case '#foundations':
-          e.target.closest('a').classList.add('active');
-          break;
-        case '#styles':
-          e.target.closest('a').classList.add('active');
-          break;
-        default:
-          document.querySelector('a[href="#home"]').classList.add('active');
-      } */
