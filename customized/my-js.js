7def735264614a14247ade8d90f2b179f919fa17
NataliaVozhdaeva <LomzNat@yandex.ru>
2024-05-16 21:28:47 +0400

    feat: add video controller

--- customized/my-js.js
+++ customized/my-js.js
@@ -1,10 +1,12 @@
+//active calss for navigation
+
 const menuItems = document.querySelectorAll('.nav-link');
 
 location.hash = 'home';
 document.querySelector('a[href="#home"]').classList.add('active');
 
 menuItems.forEach((el) => {
-  el.addEventListener('click', function appearModal(e) {
+  el.addEventListener('click', function appearModal() {
     menuItems.forEach((el) => el.classList.remove('active'));
     setTimeout(function () {
       for (let i = 0; i < menuItems.length; i++) {
@@ -15,4 +17,25 @@ menuItems.forEach((el) => {
       }
     }, 10);
   });
+});
+
+//video pause-stadt btn
+
+const videoBtnsContainer = document.querySelector('div[data-video="videoBtns"]');
+const video = document.querySelector('#hero-video');
+
+videoBtnsContainer.addEventListener('click', function VideoToggler(e) {
+  const btnPause = videoBtnsContainer.querySelector('span[data-video="pause"]');
+  const btnPlay = videoBtnsContainer.querySelector('span[data-video="play"]');
+  // console.log(e.target.dataset.video);
+
+  if (e.target.dataset.video === 'pause') {
+    video.pause();
+    btnPause.classList.add('invisible');
+    btnPlay.classList.remove('invisible');
+  } else {
+    video.play();
+    btnPause.classList.remove('invisible');
+    btnPlay.classList.add('invisible');
+  }
 });
